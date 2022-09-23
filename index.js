a750f6b40c640a166435940b8182b72e3dbc0117
xiaoyao <15558110117@163.com>
2022-09-23 15:00:27 +0800

    style: format code

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
 
-const fs = require("fs");
-const util = require("util");
-const { EOL } = require("os");
-const prompts = require("prompts");
-const gitRemoteOriginUrl = require("git-remote-origin-url");
-const parseGitUrl = require("git-url-parse");
-const execa = require("execa");
+const fs = require('fs');
+const util = require('util');
+const { EOL } = require('os');
+const prompts = require('prompts');
+const gitRemoteOriginUrl = require('git-remote-origin-url');
+const parseGitUrl = require('git-url-parse');
+const execa = require('execa');
 
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
-const RELEASE_IT_CONFIG = ".release-it.json";
-const PACKAGE_CONFIG = "package.json";
+const RELEASE_IT_CONFIG = '.release-it.json';
+const PACKAGE_CONFIG = 'package.json';
 
 (async () => {
   let manifest = {};
@@ -43,43 +43,43 @@ const PACKAGE_CONFIG = "package.json";
 
   if (remoteUrl) {
     const parsedRemoteUrl = parseGitUrl(remoteUrl);
-    isGitHub = parsedRemoteUrl.source === "github.com";
+    isGitHub = parsedRemoteUrl.source === 'github.com';
     isGitLab =
-      parsedRemoteUrl.source === "gitlab.com" ||
-      parsedRemoteUrl.resource.indexOf("gitlab") > -1;
+      parsedRemoteUrl.source === 'gitlab.com' ||
+      parsedRemoteUrl.resource.indexOf('gitlab') > -1;
   }
 
   const questions = [];
 
   if (isGitHub) {
     questions.push({
-      type: "confirm",
-      name: "github",
-      message: "Publish a GitHub Release with every release?",
+      type: 'confirm',
+      name: 'github',
+      message: 'Publish a GitHub Release with every release?',
       initial: true,
     });
   }
 
   if (isGitLab) {
     questions.push({
-      type: "confirm",
-      name: "gitlab",
-      message: "Publish a GitLab Release with every release?",
+      type: 'confirm',
+      name: 'gitlab',
+      message: 'Publish a GitLab Release with every release?',
       initial: true,
     });
   }
 
   if (hasManifest) {
     questions.push({
-      type: "select",
-      name: "config",
-      message: "Where to add the release-it config?",
+      type: 'select',
+      name: 'config',
+      message: 'Where to add the release-it config?',
       choices: [
-        { title: ".release-it.json", value: RELEASE_IT_CONFIG },
-        { title: "package.json", value: PACKAGE_CONFIG },
+        { title: '.release-it.json', value: RELEASE_IT_CONFIG },
+        { title: 'package.json', value: PACKAGE_CONFIG },
       ],
       initial: 0,
-      hint: " ",
+      hint: ' ',
     });
   }
 
@@ -101,8 +101,8 @@ const PACKAGE_CONFIG = "package.json";
 
   if (hasManifest) {
     manifest.scripts = manifest.scripts || {};
-    if (!("release" in manifest.scripts)) {
-      manifest.scripts.release = "release-it";
+    if (!('release' in manifest.scripts)) {
+      manifest.scripts.release = 'release-it';
       isManifestChanged = true;
     }
   }
@@ -113,7 +113,7 @@ const PACKAGE_CONFIG = "package.json";
   ) {
     await writeFile(
       RELEASE_IT_CONFIG,
-      JSON.stringify(config, null, "  ") + EOL
+      JSON.stringify(config, null, '  ') + EOL
     );
   } else {
     console.log(
@@ -122,14 +122,14 @@ const PACKAGE_CONFIG = "package.json";
   }
 
   if (answers.config === PACKAGE_CONFIG) {
-    manifest["release-it"] = config;
+    manifest['release-it'] = config;
     isManifestChanged = true;
   }
   if (isManifestChanged) {
-    await writeFile(PACKAGE_CONFIG, JSON.stringify(manifest, null, "  ") + EOL);
+    await writeFile(PACKAGE_CONFIG, JSON.stringify(manifest, null, '  ') + EOL);
   }
 
-  await execa("npm", ["install", "release-it", "--save-dev"], {
-    stdio: "inherit",
+  await execa('npm', ['install', 'release-it', '--save-dev'], {
+    stdio: 'inherit',
   });
 })();
